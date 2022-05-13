22a7a78e3ecd5de51ac379ebad83fadfac5af324
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-13 18:19:29 -0400

    actions-router, get- all actions and action by id

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,10 +1,36 @@
 // Write your "actions" router here!
+const express = require("express");
+const { get } = require("./actions-model");
+const { validateActionId } = require("./actions-middlware");
+const actionRouter = express.Router();
 // - [ ] `[GET] /api/actions`
 //   - Returns an array of actions (or an empty array) as the body of the response.
+actionRouter.get("/", (req, res) => {
+  get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: "No actions",
+      });
+    });
+});
 
 // - [ ] `[GET] /api/actions/:id`
 //   - Returns an action with the given `id` as the body of the response.
 //   - If there is no action with the given `id` it responds with a status code 404.
+actionRouter.get("/:id", validateActionId, (req, res) => {
+  get(req.params.id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(() => {
+      res.status(404).json({
+        message: "No existing actions with that ID",
+      });
+    });
+});
 
 // - [ ] `[POST] /api/actions`
 //   - Returns the newly created action as the body of the response.
@@ -19,3 +45,4 @@
 // - [ ] `[DELETE] /api/actions/:id`
 //   - Returns no response body.
 //   - If there is no action with the given `id` it responds with a status code 404.
+module.exports = actionRouter;
