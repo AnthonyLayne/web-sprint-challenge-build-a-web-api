85eeb608a99a5c4e555205ed7ec5b7cd10154f31
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-13 16:20:15 -0400

    code for index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
-const express = require('express');
+const express = require("express");
+const projectRouter = require("./projects/projects-router");
 const server = express();
+server.use(express.json());
+server.use("/api/projects", projectRouter);
 
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "hey" });
+});
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "not found",
+  });
+});
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
