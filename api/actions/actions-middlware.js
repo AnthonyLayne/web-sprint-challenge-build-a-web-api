9205cf570cd9ddc3d9bb7e876a136bdcaa5e650f
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-13 18:19:04 -0400

    actions-middleware, action id validation

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,22 @@
 // add middlewares here related to actions
+const ACTIONS = require("../actions/actions-model");
+
+async function validateActionId(req, res, next) {
+  try {
+    const actions = await ACTIONS.get(req.params.id);
+    if (!actions) {
+      next({ status: 404, message: "no action found" });
+    } else {
+      req.actions = actions;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "could not find action",
+    });
+  }
+}
+
+module.exports = {
+  validateActionId,
+};
